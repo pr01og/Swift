f32cb98cd18709bf0d60c1d1b530199367d142f2
«Probojnik» <probojnik@gmail.com>
2015-09-10 20:36:34 +0300

    no message

---
+++ LifeStyle/LifeStyle/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LifeStyle
+//
+//  Created by Stanislav on 10.09.15.
+//  Copyright © 2015 Stanislav. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
